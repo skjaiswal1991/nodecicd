062075a95ae4fa3dba9b60317c8872340c262d26
rateusonline <106961091+rateusonline@users.noreply.github.com>
2023-02-02 19:50:26 +0530

    update the port no

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = new express()
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Welcome back node sanjay CI CD pipe line")
 })
-app.get("/app",(req,res)=>{
+app.get("/app", (req, res) => {
     res.send("Welcome Node throw again chnage CI CD pipe line")
 })
 
-app.listen(3030,()=>{
-    console.log("3030 post start")
+app.listen(3020, () => {
+    console.log("3020 post start")
 })
